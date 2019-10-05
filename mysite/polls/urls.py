a3f1d00a5fb2910541ba4f610a7ec543f9056cf8
kuroneko <kskkm0228@gmail.com>
2019-10-05 18:39:32 +0900

    汎用ビューを使ってみた

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import SampleTemplateView
 
 urlpatterns = [
-    path(),
+    path(r'^sample$', SampleTemplateView.as_view()),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+class SampleTemplateView(TemplateView):
+    template_name = "index.html"
