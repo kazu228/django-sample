7cfe8343ef2c34fb78fcbd1c77961f12fcbe62b0
kuroneko <kskkm0228@gmail.com>
2019-10-12 16:13:06 +0900

    froms.py PersonmodelFormを追加

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -2,3 +2,10 @@ from django import forms
 
 class PersonForm(forms.Form):
     id  = forms.IntegerField(label='ID')
+
+class PersonmodelForm(forms.Form):
+    name = forms.CharField(label="名前")
+    age = forms.IntegerField(label="年齢")
+    birthday = forms.DateField(label="誕生日")
+    mail = forms.EmailField(label="メールアドレス")
+    gender = forms.BooleanField(label="性別", required=False)
