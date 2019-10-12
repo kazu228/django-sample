d22b3a0e220811ba300978095a0039d69bf18759
kuroneko <kskkm0228@gmail.com>
2019-10-12 18:02:07 +0900

    urlが変

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from .views import SampleTemplateView, MyCreateView, MyUpdateView
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
 urlpatterns = [
     path('sample', SampleTemplateView.as_view(), name='index'),
     path('sample/create', MyCreateView.as_view(), name='create'),
-    path('sample/edit/<int:pk>', MyUpdateView.as_view(), name='edit')
+    path('sample/edit/^(?P<pk>[0-9]+)/$/', MyUpdateView.as_view(), name='edit')
 ]
 
