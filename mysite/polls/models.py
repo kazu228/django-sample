5675a2e8ab2717a1f624409c0da96827397c7d67
kuroneko <kskkm0228@gmail.com>
2019-10-12 11:29:23 +0900

    models..pyにモデルをついか

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    birthday = models.DateField()
+    mail = models.EmailField(max_length=100)
+    gender = models.BooleanField()
