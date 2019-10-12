03502da32953577dcc8f8100446b378c74feb772
kuroneko <kskkm0228@gmail.com>
2019-10-12 14:34:49 +0900

    values()に引数を渡す

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,7 +11,7 @@ class SampleTemplateView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["data"] = Person.objects.all()
+        context["data"] = Person.objects.all().values('name', 'age')
         context["form"] = PersonForm()
         return context
     
