03adfb2c1e6a628ee3026316c61b25a19dad8d03
kuroneko <kskkm0228@gmail.com>
2019-10-13 18:14:41 +0900

    urlを修正

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,7 @@ from .views import find
 #from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('sample', SampleTemplateView.as_view(), name='index'),
+    path('sample/', SampleTemplateView.as_view(), name='index'),
     path('sample/create', MyCreateView.as_view(), name='create'),
     path('sample/find', find, name='find'),
     re_path('sample/edit/(?P<pk>\d+)$', MyUpdateView.as_view(), name='edit'),  #汎用viewでURLに値を渡すには、re_pathがいいかな？
